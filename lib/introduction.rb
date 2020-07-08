8eb6a649b6551302425d97b9aeeffcfa1e234bd7
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-08 00:16:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,4 @@
 def introduction (name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{Ruby}"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
+introduction (Jordan, Ruby)
